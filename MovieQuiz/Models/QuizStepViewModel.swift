ff84bb20da3b4be84ed06cab4b9665bac4e6fd88
alyonaaparina <alyonaa.malashenko@gmail.com>
2025-08-29 12:12:57 +0300

    Задача по 5 спринту

---
+++ MovieQuiz/Models/QuizStepViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  QuizStepViewModel.swift
+//  MovieQuiz
+//
+//  Created by Алена Апарина on 27.08.2025.
+//
+import Foundation
+import UIKit
+
+struct QuizStepViewModel {
+    let image: UIImage
+    let question: String
+    let questionNumber: String
+}
